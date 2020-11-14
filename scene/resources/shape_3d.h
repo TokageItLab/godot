7e29c1425cb82ab65906e14437c2ea2e59b5cea8
Tokage <tokage.it.lab@gmail.com>
2020-11-15 04:45:38 +0900

    min

--- scene/resources/shape_3d.h
+++ scene/resources/shape_3d.h
@@ -40,7 +40,7 @@ class Shape3D : public Resource {
 	OBJ_SAVE_TYPE(Shape3D);
 	RES_BASE_EXTENSION("shape");
 	RID shape;
-	real_t margin = 0.04;
+	real_t margin = 0.001;
 
 	Ref<ArrayMesh> debug_mesh_cache;
 
